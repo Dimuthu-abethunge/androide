30d29303d39fa9ca20996ede699c10770edaf57a
Dimuthu-abethunge <56988418+Dimuthu-abethunge@users.noreply.github.com>
2020-11-05 18:14:46 +0530

    StayLogIn

---
+++ SESSION/app/src/main/java/com/dimuthu/sessiontutorial/Login.java
@@ -0,0 +1,82 @@
+package com.dimuthu.sessiontutorial;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends Activity {
+
+    // Email, password edittext
+    EditText txtUsername, txtPassword;
+
+    // login button
+    Button btnLogin;
+
+    // Alert Dialog Manager
+    AlertDialogManager alert = new AlertDialogManager();
+
+    // Session Manager Class
+    SessionManager session;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Session Manager
+        session = new SessionManager(getApplicationContext());
+
+        // Email, Password input text
+        txtUsername = (EditText) findViewById(R.id.txtUsername);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
+
+        Toast.makeText(getApplicationContext(), "User Login Status: " + session.isLoggedIn(), Toast.LENGTH_LONG).show();
+
+
+        // Login button
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+
+        // Login button click event
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // Get username, password from EditText
+                String username = txtUsername.getText().toString();
+                String password = txtPassword.getText().toString();
+
+                // Check if username, password is filled
+                if(username.trim().length() > 0 && password.trim().length() > 0){
+                    // For testing puspose username, password is checked with sample data
+                    // username = test
+                    // password = test
+                    if(username.equals("test") && password.equals("test")){
+
+                        // Creating user login session
+                        // For testing i am stroing name, email as follow
+                        // Use user real data
+                        session.createLoginSession("Android Hive", "anroidhive@gmail.com");
+
+                        // Staring MainActivity
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(i);
+                        finish();
+
+                    }else{
+                        // username / password doesn't match
+                        alert.showAlertDialog(Login.this, "Login failed..", "Username/Password is incorrect", false);
+                    }
+                }else{
+                    // user didn't entered username or password
+                    // Show alert asking him to enter the details
+                    alert.showAlertDialog(Login.this, "Login failed..", "Please enter username and password", false);
+                }
+
+            }
+        });
+    }
+}
---
+++ SESSION/app/src/main/java/com/dimuthu/sessiontutorial/MainActivity.java
@@ -0,0 +1,77 @@
+package com.dimuthu.sessiontutorial;
+
+import java.util.HashMap;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    // Alert Dialog Manager
+    AlertDialogManager alert = new AlertDialogManager();
+
+    // Session Manager Class
+    SessionManager session;
+
+    // Button Logout
+    Button btnLogout;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Session class instance
+        session = new SessionManager(getApplicationContext());
+
+        TextView lblName = (TextView) findViewById(R.id.lblName);
+        TextView lblEmail = (TextView) findViewById(R.id.lblEmail);
+
+        // Button logout
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+
+        Toast.makeText(getApplicationContext(), "User Login Status: " + session.isLoggedIn(), Toast.LENGTH_LONG).show();
+
+
+        /**
+         * Call this function whenever you want to check user login
+         * This will redirect user to LoginActivity is he is not
+         * logged in
+         * */
+        session.checkLogin();
+
+        // get user data from session
+        HashMap<String, String> user = session.getUserDetails();
+
+        // name
+        String name = user.get(SessionManager.KEY_NAME);
+
+        // email
+        String email = user.get(SessionManager.KEY_EMAIL);
+
+        // displaying user data
+        lblName.setText(Html.fromHtml("Name: <b>" + name + "</b>"));
+        lblEmail.setText(Html.fromHtml("Email: <b>" + email + "</b>"));
+
+
+        /**
+         * Logout button click event
+         * */
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // Clear the session data
+                // This will clear all session data and
+                // redirect user to LoginActivity
+                session.logoutUser();
+            }
+        });
+    }
+
+}
